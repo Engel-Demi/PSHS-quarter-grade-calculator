c9acadc6331fee3f14a92d9b3d5f3368d9d1ba28
Adren Simon Heart D Tan <ashdtan@cmc.pshs.edu.ph>
2026-04-22 20:32:03 +0800

    Old code wasn't necessary.

--- calculate.py
+++
@@ -1,9 +0,0 @@
-q1 = float(input("1st quarter grade:"))
-tq2 = float(input("2nd quarter tentative grade:"))
-q2 = (q1 + tq2 * 2)/3
-tq3 = float(input("3rd quarter tentative grade:"))
-q3 = (q2 + tq3 * 2) /3
-
-#calculation for the q4 tentative
-fa1 = float(input("your fa1 score"))
-fa2 = float(input("your fa2 score"))
